c1e2f50de9dd0aa80440c445cc1d6284fc73a382
lopislabofficial-cmyk <lopislabofficial@gmail.com>
2026-05-04 00:44:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from js import Response
 
-def on_fetch(request):
-    html = """
-    <!DOCTYPE html>
-    <html lang="it">
-    <head><meta charset="UTF-8"></head>
-    <body style="background-color: #121212; color: #00b894; font-family: sans-serif; text-align: center;">
-        <h1>LopisLab Football Tips</h1>
-        <p style="color: white;">Sito Online - Pronostici in arrivo</p>
-    </body>
-    </html>
-    """
-    # Nota bene: headers con le doppie virgolette e minuscolo
-    return Response.new(html, headers={"content-type": "text/html;charset=UTF-8"})
+async def on_fetch(request, env):
+    # Costruiamo l'HTML in una variabile singola senza ritorni a capo strani
+    h = "<html><body style='background:#121212;color:#00b894;text-align:center;font-family:sans-serif;'>"
+    h += "<h1>LopisLab Football Tips</h1>"
+    h += "<p style='color:white;'>Sito Online - In attesa dei pronostici</p>"
+    h += "</body></html>"
+    
+    # Restituiamo la risposta con l'header formattato in modo ultra-semplice
+    return Response.new(h, headers={"Content-Type": "text/html"})
