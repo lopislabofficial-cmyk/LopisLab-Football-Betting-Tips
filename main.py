f9947201618450bccd26269299d4345d37cd0a30
lopislabofficial-cmyk <lopislabofficial@gmail.com>
2026-05-04 00:46:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from js import Response
+from js import Response, Headers
 
 async def on_fetch(request, env):
-    # Costruiamo l'HTML in una variabile singola senza ritorni a capo strani
-    h = "<html><body style='background:#121212;color:#00b894;text-align:center;font-family:sans-serif;'>"
-    h += "<h1>LopisLab Football Tips</h1>"
-    h += "<p style='color:white;'>Sito Online - In attesa dei pronostici</p>"
-    h += "</body></html>"
+    # Creiamo gli headers in modo esplicito
+    hdrs = Headers.new()
+    hdrs.set("Content-Type", "text/html; charset=utf-8")
     
-    # Restituiamo la risposta con l'header formattato in modo ultra-semplice
-    return Response.new(h, headers={"Content-Type": "text/html"})
+    # HTML super pulito
+    html = (
+        "<html>"
+        "<body style='background:#121212;color:#00b894;text-align:center;padding-top:50px;font-family:sans-serif;'>"
+        "<h1>LopisLab Football Tips</h1>"
+        "<p style='color:white;'>Sito Online - Test di connessione IA riuscito</p>"
+        "</body>"
+        "</html>"
+    )
+    
+    # Restituiamo la risposta usando l'oggetto Headers creato sopra
+    return Response.new(html, headers=hdrs)
