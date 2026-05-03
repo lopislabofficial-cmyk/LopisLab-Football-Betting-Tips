4a7943c860ae720fd0b7fc43f30bdd71dddb5ef6
lopislabofficial-cmyk <lopislabofficial@gmail.com>
2026-05-03 18:01:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from js import Response
 
 def on_fetch(request):
-    # Scriviamo l'HTML su una riga sola, senza f-strings o variabili intermedie
-    return Response.new(
-        "<h1>LopisLab</h1><p>Sistema Online</p><ul><li>Inter-Milan: 1X</li></ul>", 
-        headers={"Content-Type": "text/html; charset=utf-8"}
-    )
+    return Response.new("<b>LopisLab Online</b>")
