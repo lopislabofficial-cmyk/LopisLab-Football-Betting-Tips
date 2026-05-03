35a398ff29ed0f5fa89876b2aca53371c76ca86c
lopislabofficial-cmyk <lopislabofficial@gmail.com>
2026-05-03 17:36:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from js import Response
 
 def on_fetch(request):
-    html_content = """
-    <h1>LopisLab Football Tips</h1>
-    <p>Sito in fase di aggiornamento...</p>
-    <ul>
-        <li>Inter - Milan: 1X</li>
-        <li>Real - Barca: Goal</li>
-    </ul>
-    """
-    return Response.new(html_content, headers={"content-type": "text/html;charset=UTF-8"})
+    return Response.new("Sito Online - Test LopisLab")
